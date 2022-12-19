94bf4ba3ac2d969ffbe20d7cce1323ccfdc8bc98
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2022-12-19 23:02:55 +0100

    testing-clipboard-js-does-not-work-maybe-because-of-karax

--- stats/index.nim
+++ stats/index.nim
@@ -1,6 +1,19 @@
 import nimib
 
 nbInit
+nbRawHtml: """
+<script src="https://unpkg.com/clipboard@2/dist/clipboard.min.js"></script>
+<style>
+pre {
+  overflow-x: auto;
+  white-space: pre-wrap;
+  white-space: -moz-pre-wrap;
+  white-space: -pre-wrap;
+  white-space: -o-pre-wrap;
+  word-wrap: break-word;
+}
+</style>
+"""
 nbKaraxCode:
   import karax / localstorage
 
@@ -18,13 +31,18 @@ nbKaraxCode:
   let statsNewId = "statsNew"
 
   karaxHtml:
-    p:
-      text "current stats"
-    pre:
-      text getStats().kstring
-    label:
-      text "new stats"
-    textarea(id = statsNewId, placeholder = "")
+    tdiv:
+      label:
+        text "current stats"
+      pre:
+        code(lang="json"):
+          text getStats().kstring
+    button(class="btn", `data-clipboard-text` = getStats().kstring):
+      text "Copy stats"
+    tdiv:
+      label:
+        text "new stats"
+      textarea(id = statsNewId, placeholder = "")
     button:
       text "Set stats"
       proc onClick() =
